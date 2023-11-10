666549dfee93e2705cf5838523b59a7664305bb7
soongu <naong526@naver.com>
2023-11-10 11:30:46 +0900

    #2 . 여러가지 요청 처리

--- src/main/java/com/spring/mvc/chap01/ControllerV1.java
+++ src/main/java/com/spring/mvc/chap01/ControllerV1.java
@@ -1,8 +1,7 @@
 package com.spring.mvc.chap01;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -56,4 +55,36 @@ public class ControllerV1 {
         return "";
     }
 
+    // == 3. DTO(Data Tranfer Object)객체 사용하기
+    // -> 파라미터의 양이 엄청 많거나 서로 연관되어 있는 경우에 사용
+    // ex : /order?orderNum=123&goodsName=구두&amount=3&price=200000...
+    @RequestMapping("/order")
+    public String order(OrderRequestDTO dto) {
+        System.out.println("dto = " + dto);
+        System.out.println(dto.getGoodsName());
+        return "";
+    }
+
+    // == 4. URL경로에 붙어 있는 데이터 읽기
+    // ex : /member/kim/107
+
+    @GetMapping("/member/{userName}/{userNo}")
+    public String member(
+            @PathVariable String userName,
+            @PathVariable int userNo) {
+        System.out.println("userName = " + userName);
+        System.out.println("userNo = " + userNo);
+        return "";
+    }
+
+    // == 5. POST요청 데이터 읽기
+    // -> food.jsp에서 보낸 데이터를 읽을 것임
+    // 나는 POST로 오지 않으면 요청을 거절한다.
+//    @RequestMapping(value="/food-select", method = RequestMethod.POST)
+    @PostMapping("/food-select")
+    public String select(String foodName, String category) {
+        System.out.println("foodName = " + foodName);
+        System.out.println("category = " + category);
+        return "index";
+    }
 }
