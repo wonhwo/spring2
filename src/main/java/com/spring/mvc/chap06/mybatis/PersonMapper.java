cf3ff1552fcc2d1e0b220e09c1247edde5b0c7e2
soongu <naong526@naver.com>
2023-12-08 11:43:21 +0900

    #14. 마이바티스

---
+++ src/main/java/com/spring/mvc/chap06/mybatis/PersonMapper.java
@@ -0,0 +1,19 @@
+package com.spring.mvc.chap06.mybatis;
+
+
+import com.spring.mvc.chap06.entity.Person;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+// 마이바티스의 SQL실행을 위한 인터페이스
+@Mapper
+public interface PersonMapper {
+
+    // CRUD를 명세
+    boolean save(Person p); // INSERT
+    boolean update(Person p); // UPDATE
+    boolean delete(String id); // DELETE
+    List<Person> findAll(); // SELECT
+    Person findOne(String id); // SELECT
+}
